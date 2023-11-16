7ec35c4abaf2a76ee03ed861ad70da11ba4e691b
“Ian <pk20lian@gmail.com>
2023-11-16 17:16:57 -0300

    começando AP2

---
+++ AP2/script.js
@@ -0,0 +1,91 @@
+const users = [
+    { username: 'Mangeli', password: '202cb962ac59075b964b07152d234b70' }, // MD5 hash of 'password'
+    // Add more users as needed
+];
+
+document.addEventListener('DOMContentLoaded', function () {
+    const loginContainer = document.getElementById('login-container');
+    const mainContainer = document.getElementById('main-container');
+
+    const isLoggedIn = false;
+
+    if (isLoggedIn) {
+        loginContainer.style.display = 'none';
+        mainContainer.style.display = 'block';
+    } else {
+        loginContainer.style.display = 'block';
+        mainContainer.style.display = 'none';
+    }
+});
+
+function login() {
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+
+    const user = users.find(u => u.username === usernameInput.value && u.password === md5(passwordInput.value));
+
+    if (user) {
+        alert('Login efetuado!');
+        document.getElementById('login-container').style.display = 'none';
+        document.getElementById('main-container').style.display = 'block';
+    } else {
+        alert('Usuário ou senha inválido!');
+    }
+}
+
+function logout() {
+    document.getElementById('login-container').style.display = 'block';
+    document.getElementById('main-container').style.display = 'none';
+}
+
+function md5(input) {
+    return CryptoJS.MD5(input).toString();
+}
+
+const div_container = document.getElementById('atletas-container');
+
+function carregarAtletas(endpoint) {
+    document.getElementById('login-container').style.display = 'none';
+    document.getElementById('main-container').style.display = 'none';
+    document.getElementById('atletas-container').style.display = 'block';
+
+    div_container.innerHTML = '';
+
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach((atleta) => {
+                cria_cartao(atleta);
+            });
+        })
+        .catch(error => console.error('Erro ao obter os dados:', error));
+}
+
+function cria_cartao(entrada) {
+    const container_atleta = document.createElement('div');
+
+    const titulo = document.createElement('h3');
+    titulo.innerHTML = entrada.nome;
+
+    const imagem = document.createElement('img');
+    imagem.src = entrada.imagem;
+
+    container_atleta.appendChild(titulo);
+    container_atleta.appendChild(imagem);
+
+    div_container.appendChild(container_atleta);
+}
+
+
+function button1() {
+    carregarAtletas('https://botafogo-atletas.mange.li/masculino');
+}
+
+function button2() {
+    carregarAtletas('https://botafogo-atletas.mange.li/feminino');
+}
+
+function button3() {
+
+    carregarAtletas('https://botafogo-atletas.mange.li/all');
+}
